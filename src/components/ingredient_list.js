dbd9fd4d8f5a93c3a9ed643f57a0c5d6043ca520
Fer Hernandez <fh.1111@gmail.com>
2019-03-05 01:39:48 +0000

    Automatically backed up by Learn

--- src/components/ingredient_list.js
+++ src/components/ingredient_list.js
@@ -1,42 +1,11 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { removeIngredient}  from '../actions';
-import { Ingredient } from './Ingredient';
 
-class IngredientList extends Component {
-  constructor(props) {
-    super(props)
-  
-  }
-
-  handleClick(index) {
-    this.props.removeIngredient(index)
-  }
-
-  render() {
-    return (
-      <div>
-        <ul>
-          {this.props.ingredients.map((ingredient, idx) =>
-            <Ingredient
-              ingredient={ingredient}
-              key={idx}
-              onIngredientClick={this.handleClick.bind(this, idx)}
-               />
-          )}
-        </ul>
-      </div>
-  )}
+export const Ingredient = (props) => {
+  return (
+    <li
+      className='row'
+      onClick={props.onIngredientClick}>
+      {props.ingredient}
+    </li>
+  )
 }
-
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ removeIngredient}, dispatch)
-}
-
-function mapStateToProps(state) {
-  return { ingredients: state }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(IngredientList)
